55977045d54480a30e8d8994796dc2acec24480e
giacomo-degrandi <91945485+Giacomo-DeGrandi@users.noreply.github.com>
2022-03-29 11:20:21 +0200

    first commit 4 upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import { Octokit, App } from "octokit";
+import 'dotenv/config';
+
+const octokit = new Octokit({ auth: process.env.TOKEN });
+
+const {
+    data: { login },
+  } = await octokit.rest.users.getAuthenticated();
+
+const {
+    data: { license },
+} = await ocktokit.rest.repos({repo,owner})
+
+console.log("Hello, %s", login);
+
+console.log(license)
+
+/*
+const repo = 'IAMDinosaur';
+const owner = 'ivanseidel';
+
+const { data : {license, topics, language, description}} =  await octokit.rest.repos.get({
+    repo,
+    owner
+})
+
+// licences, versions, mots clés, liens web …
+// NOK collaborators
+
+console.log(license, topics, language, description)
+
+// get branches
+const { data : branches} = await octokit.request(`GET /repos/${owner}/${repo}/branches`)
+console.log(branches)
+
+// get commits descriptions
+const { data: {commit}} = await octokit.request(`https://api.github.com/repos/ivanseidel/IAMDinosaur/commits/a3737740ad6e18eb3260f9c32d1070102abc4f5e`)
+console.log(commit)
+*/
+
